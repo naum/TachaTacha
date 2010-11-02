91db3109d994f6da095f4df62fa663f70dc72562
Naum Trifanoff <aznaum@gmail.com>
2010-11-02 15:36:37 -0700

    filling in the controller

--- tachatacha.rb
+++ tachatacha.rb
@@ -5,15 +5,31 @@ DataMapper.setup(:default, "appengine://auto")
 
 class Article
   include DataMapper::Resource
+  property :id, Serial
+  property :title, String
+  property :body, Text
+  property :ctime, Integer
+  property :mtime, Integer
 end
 
 helpers do
   include Rack::Utils
+  alias_method :e, :escape
   alias_method :h, :escape_html
+  alias_method :u, :unescape
 end
 
 get '/' do
   erb :index
+end
+
+get '/edit/:page' do
+end
+
+get '/wiki/:page' do
+end
+
+post '/edit' do
 end
 
 get '/stirup' do
